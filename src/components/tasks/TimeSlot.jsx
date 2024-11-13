8ff1ff6a623ae86313431551fc758e602c3626dc
James Smith <dev.jpsmith@gmail.com>
2024-11-13 15:33:36 -0700

    added hover to see full title

--- src/components/tasks/TimeSlot.jsx
+++ src/components/tasks/TimeSlot.jsx
@@ -7,8 +7,9 @@ function TimeSlot({time, agendaItem}) {
             <p className="text-md text-gray-400 p-2 w-16">{time}</p>
             {agendaItem && (
                 <div className={`grid grid-cols-4 w-full`}>
-                    <div className={`${units} ${color} p-2 rounded-lg border border-gray-400`}>
+                    <div data-tooltip-target="title_tooltip" className={`${units} ${color} group relative p-2 rounded-lg border border-gray-400`}>
                         <p className="text-md text-gray-600 text-nowrap truncate">{title}</p>
+                        <div className="absolute top-[-15px] left-1/3 bg-gray-100 rounded-md invisible p-2 opacity-75 group-hover:visible">{title}</div>
                         <p className="text-sm text-gray-400">{from}-{to}</p>
                     </div>
                 </div>
